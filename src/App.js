4bdc83c018688e64f01923adc0981a83d5f4d881
Simone Rescio <simone.rescio@gmail.com>
2024-06-02 14:21:57 +0200

    button will save to localstorage and restore on reload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import Header from './Components/Header';
+import './App.css';
 
 function App() {
+  const [yaml, setYaml] = useState('');
+
+  useEffect(() => {
+    const savedValue = localStorage.getItem('yaml-input');
+    if (savedValue) {
+      setYaml(savedValue);
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header />
+      <textarea
+        id="yaml-input"
+        placeholder="Paste your YAML template here"
+        value={yaml}
+        onChange={e => setYaml(e.target.value)}
+      />
+      <button onClick={() => localStorage.setItem('yaml-input', yaml)}>
+        ya!
+      </button>
     </div>
   );
 }
