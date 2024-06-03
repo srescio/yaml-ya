7381e8cbd10cc6eb7c5e32be1e6725e4a47955bd
Simone Rescio <simone.rescio@gmail.com>
2024-06-03 22:24:51 +0200

    more tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -87,6 +87,84 @@ describe('yaml ya', () => {
     cy.get('button#yaml-delete').should('contain', name);
   })
 
+  it('saved templates should persist after reload', () => {
+    const name = 'name-test';
+    cy.land()
+    cy.fillAndYa(name)
+    cy.reload()
+    cy.get('select').should('exist')
+    cy.get('select').should('have.value', name);
+  })
+
+  it('reset button should deselect template and reset default values', () => {
+    const name = 'name-test';
+    cy.land()
+    cy.fillAndYa(name)
+    cy.get('button#yaml-reset').click()
+    cy.get('select').should('have.value', '');
+    cy.get('input[name="yaml-name"]').should('have.value', 'yaml_tpl');
+    cy.get('input[name="yaml-replace"]').should('have.value', '$');
+    cy.get('input[name="yaml-repeat"]').should('have.value', '2');
+    cy.get('textarea#yaml-input').should('have.value', '');
+    cy.get('button#yaml-ya').should('be.disabled');
+    cy.get('pre').should('not.exist');
+  })
+
+  it('delete button should reset default values and make select disappear when only config is gone', () => {
+    const name = 'name-test';
+    cy.land()
+    cy.fillAndYa(name)
+    cy.get('button#yaml-delete').click()
+    cy.get('select').should('not.exist');
+    cy.get('button#yaml-export').should('not.exist');
+    cy.get('input[name="yaml-name"]').should('have.value', 'yaml_tpl');
+    cy.get('input[name="yaml-replace"]').should('have.value', '$');
+    cy.get('input[name="yaml-repeat"]').should('have.value', '2');
+    cy.get('textarea#yaml-input').should('have.value', '');
+    cy.get('button#yaml-ya').should('be.disabled');
+    cy.get('pre').should('not.exist');
+  })
+
+  it('deleted template should not be pre-selected after reload', () => {
+    const name = 'name-test';
+    cy.land()
+    cy.fillAndYa(name)
+    cy.get('button#yaml-delete').click()
+    cy.reload()
+    cy.get('select').should('not.exist');
+  })
+
+  it('template selection should update related fields', () => {
+    const name = 'name-test';
+    cy.land()
+    cy.fillAndYa(name)
+    cy.get('textarea#yaml-input').clear().type('yaml&test')
+    cy.get('input[name="yaml-name"]').clear().type('name-test2')
+    cy.get('input[name="yaml-replace"]').clear().type('&')
+    cy.get('input[name="yaml-repeat"]').clear().type('3')
+    cy.get('button#yaml-ya').click()
+    cy.get('select').select(name)
+    cy.get('input[name="yaml-name"]').should('have.value', name);
+    cy.get('input[name="yaml-replace"]').should('have.value', '£');
+    cy.get('input[name="yaml-repeat"]').should('have.value', '4');
+    cy.get('textarea#yaml-input').should('have.value', 'yaml£test');
+  })
+
+  it('should update template config if saving with name of a saved template', () => {
+    const name = 'name-test';
+    cy.land()
+    cy.fillAndYa(name)
+    cy.get('input[name="yaml-name"]').clear().type(name)
+    cy.get('input[name="yaml-repeat"]').clear().type('3')
+    cy.get('button#yaml-ya').click()
+    cy.get('button#yaml-reset').click();
+    cy.get('select').select(name)
+    cy.get('input[name="yaml-name"]').should('have.value', name);
+    cy.get('input[name="yaml-replace"]').should('have.value', '£');
+    cy.get('input[name="yaml-repeat"]').should('have.value', '3');
+    cy.get('textarea#yaml-input').should('have.value', 'yaml£test');
+  })
+
   describe('when loaded with saved templates', () => {
     it('should display the select template dropdown and export buttons', () => {
       cy.visit('/')
